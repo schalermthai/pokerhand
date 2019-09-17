9b81e0ba3658a4c6ce16723cd6031e50a7746908
schalerm <schalerm@thoughtworks.com>
2019-09-17 23:55:36 +0700

    two pairs should have 1 single as well

--- pokerhand-fp/src/ranks/twoPairs.ts
+++ pokerhand-fp/src/ranks/twoPairs.ts
@@ -1,11 +1,11 @@
 import { Matcher } from '~/matchers/type'
 import { rankMatcher } from '~/matchers/rank'
-import { double } from '~/matchers/repeats'
+import {double, single} from '~/matchers/repeats'
 import { Hand } from '~/card'
 import { faces } from '~/matchers/composites/groups'
 
 export const twoPairs: Matcher<Hand> = rankMatcher(
   'Two Pairs',
   3,
-  faces(double(2))
+  faces(double(2), single(1))
 )
