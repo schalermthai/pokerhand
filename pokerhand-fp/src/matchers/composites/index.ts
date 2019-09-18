00a9543eef607e23deb84d5d658a53fa657f382c
schalerm <schalerm@thoughtworks.com>
2019-09-18 11:31:15 +0700

    clean up composite type

--- pokerhand-fp/src/matchers/composites/index.ts
+++ pokerhand-fp/src/matchers/composites/index.ts
@@ -1,9 +1,9 @@
-import { Matcher, MatchingResult } from '~/matchers/type'
+import { Matcher } from '~/matchers/type'
 import * as _ from 'lodash/fp'
 
-export const matchers: <T>(
-  ...matchers: Matcher<T>[]
-) => (t: T) => MatchingResult = (...matchers) => t => ({
+export const matchers: <T>(...matchers: Matcher<T>[]) => Matcher<T> = (
+  ...matchers
+) => t => ({
   result: matchers.every(m => m(t).result),
   matched: _.head(matchers)(t).matched,
   unmatched: _.head(matchers)(t).unmatched
