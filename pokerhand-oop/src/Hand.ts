99d4f14ab78cfc8e392fc5ef8f6e6984392d8cf3
schalerm <schalerm@thoughtworks.com>
2019-09-22 17:00:06 +0700

    add more test for high card cases

--- pokerhand-oop/src/Hand.ts
+++ pokerhand-oop/src/Hand.ts
@@ -80,3 +80,5 @@ export class Hand {
     })
   }
 }
+
+['']
--- pokerhand-oop/test/ranks/HighCard.spec.ts
+++ pokerhand-oop/test/ranks/HighCard.spec.ts
@@ -13,4 +13,40 @@ describe('HighCard', () => {
   it('matches as a high card', () => {
     expect(h.rank.name).toEqual('High Card')
   })
+
+  it('does not match straight', () => {
+    const h = new Hand([
+      new Card(Face._2, Suit.S),
+      new Card(Face._3, Suit.C),
+      new Card(Face._4, Suit.S),
+      new Card(Face._5, Suit.S),
+      new Card(Face._6, Suit.S)
+    ])
+
+    expect(h.rank.name).not.toEqual('High Card')
+  })
+
+  it('does not match flush', () => {
+    const h = new Hand([
+      new Card(Face._2, Suit.S),
+      new Card(Face._7, Suit.S),
+      new Card(Face._4, Suit.S),
+      new Card(Face._6, Suit.S),
+      new Card(Face._8, Suit.S)
+    ])
+
+    expect(h.rank.name).not.toEqual('High Card')
+  })
+
+  describe('duel', () => {
+    const h2 = new Hand([
+      new Card(Face._2, Suit.S),
+      new Card(Face._7, Suit.C),
+      new Card(Face._5, Suit.S),
+      new Card(Face._6, Suit.S),
+      new Card(Face._8, Suit.S)
+    ])
+
+    expect(h2.duel(h)).toEqual('You win: High Card with 5 > 4')
+  })
 })
